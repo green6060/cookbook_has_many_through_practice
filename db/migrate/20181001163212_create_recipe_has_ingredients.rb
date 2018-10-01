ff01fef21afea2e6d76b53bf8f0367ecb5a6cf25
Dallas Parker <dallasjamesparker@gmail.com>
2018-10-01 10:33:05 -0600

    initial

---
+++ db/migrate/20181001163212_create_recipe_has_ingredients.rb
@@ -0,0 +1,11 @@
+class CreateRecipeHasIngredients < ActiveRecord::Migration[5.2]
+  def change
+    create_table :recipe_has_ingredients do |t|
+      t.belongs_to :recipe, foreign_key: true
+      t.belongs_to :ingredient, foreign_key: true
+      t.string :amount
+
+      t.timestamps
+    end
+  end
+end
